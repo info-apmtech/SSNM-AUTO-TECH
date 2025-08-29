473450d5ac6b202534c8e9ccb4853776e7d98d6f
harini232 <harini.renganathan02@gmail.com>
2025-08-29 13:38:01 +0530

    pushing files

---
+++ SSNWebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SSNWebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
